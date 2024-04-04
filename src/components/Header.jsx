62768beaa3c45747f66a80764585e06431e7c8c8
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-04 18:01:33 +0600

    Active class added in Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import NavBar from './NavBar';
 
 const Header = () => {
   return (
-    <header className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+    <header className='navbar mb-12 shadow-lg bg-neutral text-neutral-content sticky top-0 z-10'>
       <div className='container mx-auto'>
         <NavBar />
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,24 @@ import { NavLink } from 'react-router-dom';
 import Logo from './BrandLogo';
 
 const NavBar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? 'btn btn-ghost btn-sm rounded-btn btn-active'
+      : 'btn btn-ghost btn-sm rounded-btn';
+
   return (
     <nav className='flex items-center px-2 mx-2 w-full'>
       <Logo />
 
       <div className='flex-1 px-2 mx-2'>
-        <ul className='flex justify-end'>
+        <ul className='flex justify-end gap-2'>
           <li>
-            <NavLink to='/' className={`btn btn-ghost btn-sm rounded-btn`}>
+            <NavLink to='/' className={linkClass}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to='/about' className={`btn btn-ghost btn-sm rounded-btn`}>
+            <NavLink to='/about' className={linkClass}>
               About
             </NavLink>
           </li>
