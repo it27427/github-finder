8239fde5b2eaa1ec68fdfdf36a8be523c40fa02a
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-04 19:56:25 +0600

    About Page Completed

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className='px-2'>
+      <h1 className='text-4xl md:text-5xl lg:text-6xl mb-4 text-white'>
+        Github Finder
+      </h1>
+      <p className='mb-4 text-2xl font-light text-gray-200'>
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className='text-lgtext-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
     </div>
   );
 };
