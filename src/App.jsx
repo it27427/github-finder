d9bfa1b94f0bcbfe57e981e8ad860ef92dfa3a0e
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-04 19:45:45 +0600

    App, Error and Navbar component path updated by alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 import HomePage from '@/pages/HomePage';
 import AboutPage from '@/pages/AboutPage';
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import NotFound from '../assets/images/not-found.svg';
+import NotFound from '@/assets/images/not-found.svg';
 import { FaArrowLeft } from 'react-icons/fa';
 
 const Error = () => {
