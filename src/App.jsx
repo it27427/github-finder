4eb0449b98e9791753a1445f679bdfaf1bdabf56
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-05 16:32:10 +0600

    Configure The App Component By GithubProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import HomePage from '@/pages/HomePage';
 import AboutPage from '@/pages/AboutPage';
 import NotFoundPage from '@/pages/NotFoundPage';
 
+import { GithubProvider } from '@/context/github/GithubContext';
+
 /*
  * NOTE: Alert is only used on the '/' route moving to that route we can prevent
  * Content shift when alert shows by hiding and unhiding the Alert rather than
@@ -14,23 +16,25 @@ import NotFoundPage from '@/pages/NotFoundPage';
 
 const App = () => {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Header />
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Header />
 
-        <main className='px-3 pb-12'>
-          <div className='container mx-auto'>
-            <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='/about' element={<AboutPage />} />
-              <Route path='/*' element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </main>
+          <main className='px-3 pb-12'>
+            <div className='container mx-auto'>
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/about' element={<AboutPage />} />
+                <Route path='/*' element={<NotFoundPage />} />
+              </Routes>
+            </div>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 };
 
