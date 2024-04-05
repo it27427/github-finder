03e24c040d43bdf1129034476445f51e0e270e6c
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-05 16:17:44 +0600

    Display Users

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,9 +1,27 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const UserItem = ({ user }) => {
+const UserItem = ({ user: { login, avatar_url } }) => {
   return (
-    <div>
-      <h3>User Item</h3>
+    <div className='card shadow-md compact side bg-gray-800'>
+      <div className='flex-row items-center space-x-4 card-body'>
+        <div>
+          <div className='avatar'>
+            <figure className='rounded-full shadow w-14 h-14'>
+              <img src={avatar_url} alt='Profile-Image' />
+            </figure>
+          </div>
+        </div>
+        <div>
+          <h2 className='card-title text-white'>{login}</h2>
+          <Link
+            to={`/users/${login}`}
+            className='text-base-content text-opacity-60'
+          >
+            Visit Profile
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
