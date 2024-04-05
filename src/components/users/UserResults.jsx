e3989d967f0e650971b9503d6feb0f97f3b8a8c3
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-05 14:39:14 +0600

    FetchUsers from Github-API

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 const UserResults = () => {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(
+      `${import.meta.env.REACT_APP_GITHUB_URL}/users`,
+      {
+        headers: {
+          Authorization: `token ${import.meta.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    const data = response.json();
+    console.log(data);
+  };
+
   return (
     <div>
       <h1>User Results</h1>
