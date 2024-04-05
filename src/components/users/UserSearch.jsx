6dd9efb149ba42efa348d36ca36ee44c880ea755
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-05 19:46:16 +0600

    search-form added

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 
 const UserSearch = () => {
   return (
-    <div>
-      <h1>User Search Component</h1>
+    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8 mb-8'>
+      <div>
+        <form>
+          <div className='form-control'>
+            <div className='relative'>
+              <input
+                type='text'
+                className='w-full pr-20 lg:pr-40 bg-gray-200 input input-lg text-black'
+                placeholder='Search...'
+              />
+              <button
+                type='submit'
+                className='absolute top-0 right-0 btn btn-lg w-24 lg:w-40 rounded-l-none'
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div>
+        <button className='btn btn-ghost btn-lg'>Clear</button>
+      </div>
     </div>
   );
 };
