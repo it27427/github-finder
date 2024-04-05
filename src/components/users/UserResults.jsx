207058abd0f3e04d38dc5a1ee407079248944500
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-05 14:49:22 +0600

    add loading in userlist-component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const UserResults = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -16,14 +19,21 @@ const UserResults = () => {
     );
 
     const data = response.json();
-    console.log(data);
+    setUsers(data);
+    setLoading(false);
   };
 
-  return (
-    <div>
-      <h1>User Results</h1>
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <h3>Loading...</h3>;
+  }
 };
 
 export default UserResults;
