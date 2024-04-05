f34173f84da49543521f5ca908e91afd0bac9ab5
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-05 19:59:44 +0600

    handleInputValue and handleSubmitForm Adding

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 
 const UserSearch = () => {
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => setText(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8 mb-8'>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='form-control'>
             <div className='relative'>
               <input
                 type='text'
                 className='w-full pr-20 lg:pr-40 bg-gray-200 input input-lg text-black'
                 placeholder='Search...'
+                value={text}
+                onChange={handleChange}
               />
               <button
                 type='submit'
