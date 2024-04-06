c15fb2e05712d0afa24b6ef28aa4c3f1e70eaf6d
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-06 12:23:03 +0600

    setAlert

--- src/context/alert/AlertContext.jsx
+++ src/context/alert/AlertContext.jsx
@@ -8,6 +8,16 @@ export const AlertProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(alertReducer, initialState);
 
+  // SET AN-ALERT
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: 'SET_ALERT',
+      payload: { msg, type },
+    });
+
+    setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000);
+  };
+
   return (
     <AlertContext.Provider
       value={{
