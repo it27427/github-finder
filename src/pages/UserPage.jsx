f4819d7a5d3ca84bd97e7d66d738a42de5e5f919
it27427 <istiak.tushar.qtec@gmail.com>
2024-04-06 14:40:28 +0600

    Spinner added in UserPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useContext } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
+import Spinner from '@/components/layouts/Spinner';
 import GithubContext from '@/context/github/GithubContext';
-import { useParams } from 'react-router-dom';
 
 const UserPage = () => {
-  const { getUser, user } = useContext(GithubContext);
+  const { getUser, user, loading } = useContext(GithubContext);
   const params = useParams();
 
   useEffect(() => {
     getUser(params.login);
   }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <>
